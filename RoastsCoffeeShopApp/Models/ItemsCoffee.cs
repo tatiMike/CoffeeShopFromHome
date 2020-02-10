6e24cce62578b717a95345ec109e8b629ce0d7cb
M B <mbon419@gmail.com>
2020-02-10 15:07:36 -0500

    first commit

---
+++ RoastsCoffeeShopApp/Models/ItemsCoffee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoastsCoffeeShopApp.Models
+{
+    public partial class ItemsCoffee
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
